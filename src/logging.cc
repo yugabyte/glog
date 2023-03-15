ec10f5a9a64f32dac49ab5bdf933244892ee6375
Hari Krishna Sunder <hari90@users.noreply.github.com>
2023-03-15 14:06:42 -0700

    Fix empty string

--- src/logging.cc
+++ src/logging.cc
@@ -1321,7 +1321,7 @@ void LogMessage::Init(const char* file,
   //    (log level, GMT month, date, time, thread_id, file basename, line)
   // We exclude the thread_id for the default thread.
   if (FLAGS_log_prefix && (line != kNoLogPrefix)) {
-    stream() << (log_prefix_func ? log_prefix_func() : '')
+    stream() << (log_prefix_func ? log_prefix_func() : "")
              << LogSeverityNames[severity][0]
              << setw(2) << 1+data_->tm_time_.tm_mon
              << setw(2) << data_->tm_time_.tm_mday
